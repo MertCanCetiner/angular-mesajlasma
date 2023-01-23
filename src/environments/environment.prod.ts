780c26d8635c3386affe087361ff303033d851e3
Mert Can Çetiner <darkloredsez@outlook.com>
2023-01-23 13:01:04 +0300

    angular

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'mertangular',
+    appId: '1:415605908445:web:eb38f5b8268a52d5da8c34',
+    storageBucket: 'mertangular.appspot.com',
+    apiKey: 'AIzaSyBYF7aMTpnFnRMsIvaQNqcmBi2yWkLwYec',
+    authDomain: 'mertangular.firebaseapp.com',
+    messagingSenderId: '415605908445',
+    measurementId: 'G-59ZNL8SQ88',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'mertangular',
+    appId: '1:415605908445:web:eb38f5b8268a52d5da8c34',
+    storageBucket: 'mertangular.appspot.com',
+    apiKey: 'AIzaSyBYF7aMTpnFnRMsIvaQNqcmBi2yWkLwYec',
+    authDomain: 'mertangular.firebaseapp.com',
+    messagingSenderId: '415605908445',
+    measurementId: 'G-59ZNL8SQ88',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
